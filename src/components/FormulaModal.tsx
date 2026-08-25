fb6a16f9fc5ecba49cc2d7e91ad81fc4fd12dd20
Maruf Hussen <marufhussen50414@gmail.com>
2026-08-25 13:29:39 +0200

    Update FormulaModal.tsx

--- src/components/FormulaModal.tsx
+++ src/components/FormulaModal.tsx
@@ -135,7 +135,7 @@ export const FormulaModal: React.FC<FormulaModalProps> = ({ isOpen, onClose }) =
                 <span className="font-bold text-red-600">TOTALE TRATTENUTE</span> ={' '}
                 (<span className="font-bold text-purple-600">IRPEF + IMP. SOST.</span>) +{' '}
                 <span className="font-bold text-amber-700">TOTALE CONTRIBUTI</span> +{' '}
-                <span className="font-bold text-orange-600">TRATTENUTE</span>
+                <span className="font-bold text-orange-600">ADDIZIONALI</span>
               </div>
             </div>
 
@@ -147,17 +147,17 @@ export const FormulaModal: React.FC<FormulaModalProps> = ({ isOpen, onClose }) =
               <div className="space-y-2 text-xs font-mono">
                 <div className="bg-white p-2.5 rounded border border-gray-200">
                   <span className="font-semibold text-gray-700">To calculate IRPEF + IMP. SOST.:</span>
-                  <div className="text-gray-900 mt-1">IRPEF + IMP. SOST. = TOTALE TRATTENUTE - TOTALE CONTRIBUTI - TRATTENUTE</div>
+                  <div className="text-gray-900 mt-1">IRPEF + IMP. SOST. = TOTALE TRATTENUTE - TOTALE CONTRIBUTI - ADDIZIONALI</div>
                 </div>
 
                 <div className="bg-white p-2.5 rounded border border-gray-200">
                   <span className="font-semibold text-gray-700">To calculate TOTALE CONTRIBUTI:</span>
-                  <div className="text-gray-900 mt-1">TOTALE CONTRIBUTI = TOTALE TRATTENUTE - (IRPEF + IMP. SOST.) - TRATTENUTE</div>
+                  <div className="text-gray-900 mt-1">TOTALE CONTRIBUTI = TOTALE TRATTENUTE - (IRPEF + IMP. SOST.) - ADDIZIONALI</div>
                 </div>
 
                 <div className="bg-white p-2.5 rounded border border-gray-200">
-                  <span className="font-semibold text-gray-700">To calculate TRATTENUTE:</span>
-                  <div className="text-gray-900 mt-1">TRATTENUTE = TOTALE TRATTENUTE - (IRPEF + IMP. SOST.) - TOTALE CONTRIBUTI</div>
+                  <span className="font-semibold text-gray-700">To calculate ADDIZIONALI:</span>
+                  <div className="text-gray-900 mt-1">ADDIZIONALI = TOTALE TRATTENUTE - (IRPEF + IMP. SOST.) - TOTALE CONTRIBUTI</div>
                 </div>
               </div>
             </div>
