6b92024bf25f2f5ef6e6ab9f5c2c9da31e4857fb
Maruf Hussen <marufhussen50414@gmail.com>
2026-08-26 12:03:27 +0200

    Update BustaPaga.tsx

--- src/components/BustaPaga.tsx
+++ src/components/BustaPaga.tsx
@@ -31,12 +31,14 @@ const CUSTOM_FIELD_TITLES: Record<string, string> = {
   'contributi_anno': 'গত মাসের CONTRIBUTI ANNO + চলতি মাসের (INPS + FIS) কন্ট্রিবিউশন:',
 
   // 20. IMPONIBILE FISCALE (Anno)
-  '20_imponibile_fiscale_anno': 'গত মাসের IMPONIBILE FISCALE (ANNO) + চলতি মাসের IMPONIBILE FISCALE (MESE):',
-  'imponibile_fiscale_anno': 'গত মাসের IMPONIBILE FISCALE (ANNO) + চলতি মাসের IMPONIBILE FISCALE (MESE):',
+  '20_imponibile_fiscale_anno': 'গত মাসের IMPONIBILE FISCALE (ANNO) + চলতি মাসের IMPONIBILE FISCALE (MESE) এর মান দিন:',
+  'imponibile_fiscale_anno': 'গত মাসের IMPONIBILE FISCALE (ANNO) + চলতি মাসের IMPONIBILE FISCALE (MESE) এর মান দিন:',
 
-  // 22. DETR. LAV. DIPENDENTE (Anno)
-  '22_detr_lav_dip_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE):',
-  'detr_lav_dip_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE):',
+  // 17/22. DETR. LAV. DIPENDENTE (Anno)
+  '22_detr_lav_dip_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE) এর মান দিন:',
+  'detr_lav_dip_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE) এর মান দিন:',
+  '17_detr_lav_dipendente_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE) এর মান দিন:',
+  'detr_lav_dipendente_anno': 'গত মাসের DETR. LAV. DIPENDENTE (ANNO) + চলতি মাসের DETR. LAV. DIPENDENTE (MESE) এর মান দিন:',
 
   // 34. TFR ANNUO PROGR.
   '34_tfr_annuo_progr': 'গত মাসের TFR ANNUO PROGR এবং চলতি মাসের TFR MESE এর মান দিন:',
